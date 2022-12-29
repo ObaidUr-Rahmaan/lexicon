29d2c2ee46e8c96cac52c23c98cc186fd6d05c97
ObaidUr-Rahmaan <Obaid.UrRahmaan@vonage.com>
2022-12-29 00:00:08 +0000

    Attempt to Dockerize

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,6 @@ const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const mysql = require('mysql');
-const dotenv = require('dotenv');
-
-// Load the environment variables from the .env file
-dotenv.config();
 
 app.use(cors());
 
@@ -20,31 +15,8 @@ const io = new Server(server, {
     }
 });
 
-// Create a connection to the MySQL database using the environment variables
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
-// Connect to the MySQL database
-connection.connect();
-
 io.on('connection', (socket) => {
     console.log('a user connected');
-
-    // Listen for the 'create room' event
-    socket.on('create room', (roomName) => {
-        console.log(`Creating room ${roomName}`);
-        // Create a new room in the MySQL database
-        connection.query(`INSERT INTO rooms (name, size) VALUES (?, 5)`, [roomName], (error, results) => {
-            if (error) throw error;
-            // Join the room
-            socket.join(roomName);
-            console.log(`${socket.id} joined room ${roomName}`);
-        });
-    });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
